4af6b478796ef744181dce9de74b20623c833f38
saeedhyder <saeedhyder@axact.com>
2017-11-30 14:37:51 +0500

    Signed-off-by: saeedhyder <saeedhyder@axact.com>

--- app/src/main/java/com/ingic/tanfit/range_bar/SimpleRangeView.kt
+++ app/src/main/java/com/ingic/tanfit/range_bar/SimpleRangeView.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.ingic.tanfit.fragments
+package com.ingic.tanfit.range_bar;
 
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
